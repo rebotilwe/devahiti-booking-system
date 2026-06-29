06de9198bb42507196c75f17826ad17ac993d846
Rebotilwe <rebotilwemokiba@gmail.com>
2026-06-29 08:37:02 +0200

    image

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import animate from 'tailwindcss-animate'
+
 export default {
   darkMode: ["class"],
   content: [
@@ -94,5 +96,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 }
