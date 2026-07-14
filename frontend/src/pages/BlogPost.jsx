1fea03f7e97a262a370b5637884eee63c7a2387b
Rebotilwe <rebotilwemokiba@gmail.com>
2026-07-14 15:42:27 +0200

    rest

--- frontend/src/pages/BlogPost.jsx
+++ frontend/src/pages/BlogPost.jsx
@@ -1,7 +1,7 @@
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Phone, ShoppingBag, Menu, X, Calendar, Clock, ArrowLeft } from "lucide-react";
-import heroBgImg from "../assets/images/yoga.png";
+import heroBgImg from "../assets/images/rest.jpg";
 import logo from "../assets/devahiti.png";
 
 const API_URL = "https://devahiti-booking-system.onrender.com/api";
